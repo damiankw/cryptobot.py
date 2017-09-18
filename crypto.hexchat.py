e2300b5107a0084c11275eb5fa1d20837aa77ae7
Damian West <damian@damian.id.au>
2017-09-19 00:24:16 +1000

    Updated Hexchat script, found bugs with input and output.

--- crypto.hexchat.py
+++ crypto.hexchat.py
@@ -16,7 +16,10 @@ import urllib.request
 #from imp import reload
 
 def alias_crypto(word, word_eol, userdata):
-  text = word_eol[1]
+  if (len(word_eol) > 1):
+    text = word_eol[1]
+  else:
+    text = ''
   cur_list = {"AUD", "BRL", "CAD", "CHF", "CLP", "CNY", "CZK", "DKK", "EUR", "GBP", "HKD", "HUF", "IDR", "ILS", "INR", "JPY", "KRW", "MXN", "MYR", "NOK", "NZD", "PHP", "PKR", "PLN", "RUB", "SEK", "SGD", "THB", "TRY", "TWD", "ZAR"}
   cur = None
   cry = None
@@ -48,7 +51,7 @@ def alias_crypto(word, word_eol, userdata):
     print("\x16SYM    NAME                MARKET CAP         PRICE(%s)              SUPPLY     1HR      1D      7D\x16" % cur)
     for item in items:
       print("{0:6} {1:19} ${2:16,.0f}  $ {3:11,.4f} {4:16,.0f}  {5:6}  {6:6}  {7:6}".format(item['symbol'], item['name'], float(item['market_cap_' + cur.lower()]), float(item['price_' + cur.lower()]), float(item['total_supply']), float(item['percent_change_1h']), float(item['percent_change_24h']), float(item['percent_change_7d'])))
-      print("*** End of List ***")
+    print("*** End of List ***")
     
   return hexchat.EAT_ALL
 
@@ -56,7 +59,6 @@ def get_crypto(cur="AUD", limit=15, cry=None):
 
   if (cry == None):
     http = urllib.request.urlopen('https://api.coinmarketcap.com/v1/ticker/?convert=%s&limit=%s' % (cur, limit))
-    print('https://api.coinmarketcap.com/v1/ticker/?convert=%s&limit=%s' % (cur, limit))
     html = http.read().decode('utf-8')
     items = json.loads(html)
 
